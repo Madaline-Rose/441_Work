a79ce57be880b972f23a9f1db767cf7769d64a08
Madaline-Rose <madgeek3116@gmail.com>
2026-02-04 01:05:18 +0900

    Inputting my HTML and JavaScript

--- HW3/script.js
+++ HW3/script.js
@@ -1,1 +1,30 @@
 
+/*
+    Console log homework
+    This file logs personal favorites and shows
+    my use of comments, console logging, and prompt
+*/
+
+// Favorite websites
+console.log("Favorte Websites:");
+console.log("Youtube");
+console.log("Steam");
+console.log("Pintrest");
+
+// Favorite Games
+console.log("Favorite Games");
+console.log("Dishonored 2");
+console.log("NieR: Automata");
+console.log("The Last of Us");
+
+// Favorite Artists
+console.log("Favorite artists and their cultural importance");
+console.log("Hayao Miyazaki - elevated animation and explored themes of nature, pacifism, humanity, and human connection in a magical way");
+console.log("Banksy - challenged traditional art instiutions by bringing political and social commentary into public spaces");
+console.log("Marina Abramovic - pushed the boundaries of performance art by using the body to explore endurance, vulnerability, and the relationship between artist and audience");
+
+// Prompt Question
+let answer = prompt("What is your comfort movie/show?");
+
+//User Response Log
+console.log("User's comfort movie/show:", answer);
